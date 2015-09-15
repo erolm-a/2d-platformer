5c738942d8f9afebb7049064aabf2cacdbb8959a
Enrico Pranieri <lupus_in_fabula@libero.it>
2015-09-15 09:26:39 +0200

    Added checkpoints

---
+++ Platformer_v0-0/game/flag.h
@@ -0,0 +1,33 @@
+#ifndef FLAG_H
+#define FLAG_H
+
+// flag: la bandiera checkpoint
+
+#include "game_instance_generic.h"
+#include "gfx/spr_vec.h"
+#include "game/player.h"
+
+class flag : public game_instance_generic
+{
+    bool done {false};
+public:
+    void spawn(int x, int y) override
+    {
+        own_sprite = spr_vec::new_add_sprite("res/flag.png", 2);
+        own_sprite->x = x, own_sprite->y = y;
+        own_sprite->set_frame(0);
+    }
+    void handle_collision(game_instance_generic& other) override
+    {
+        if(!done && typeid(other) == typeid(player)) {
+            dynamic_cast<player&>(other).checkpoint = {.x = own_sprite->x,
+                                                       .y =own_sprite->y};
+            own_sprite->set_frame(1);
+            done = true;
+        }
+    }
+
+
+};
+
+#endif // FLAG_H
